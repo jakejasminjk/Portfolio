11de07a453da95cc0515fc462c6dc3362989f542
Kervens Jasmin <jakejasminjk@gmail.com>
2018-01-03 23:18:42 +0000

    First attempt to deploy. Everything is working as it should

---
+++ app.js
@@ -0,0 +1,38 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser");
+    
+   
+    app.set("view engine", "ejs");
+    
+    app.get("/", function(req, res){
+        res.render("landing")
+    })
+    
+    app.get("/home",function(req, res){
+        
+        res.render("home");
+    })
+    
+    app.get("/hobbies", function(req, res){
+        
+        res.render("hobbies");
+        
+    })
+    
+    app.get("/contact", function(req, res){
+        res.render("contact");
+    })
+    
+    app.get("/work", function(req, res){
+        res.render("work");
+    })
+    
+   app.get("/image",function(req,res){
+       res.redirect("https://secure-bastion-40206.herokuapp.com/");
+   })
+    //Start server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Project is up");
+});
+
