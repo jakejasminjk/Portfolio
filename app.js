14ee0556c827aaa6db4e58457665b88bf48c3c23
Kervens Jasmin <jakejasminjk@gmail.com>
2018-04-04 04:38:25 +0000

    Just fixed some links on the work page

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 var express     = require("express"),
     app         = express(),
+    fs          = require("fs"),
     bodyParser  = require("body-parser");
     
    
-    app.set("view engine", "ejs");
+    app.set('view engine', 'ejs');
     
-    app.get("/", function(req, res){
-        res.render("landing")
+    app.get('/', function(req, res){
+        res.render('landing')
     })
     
-    app.get("/home",function(req, res){
+    app.get('/home',function(req, res){
         
-        res.render("home");
+        res.render('home');
     })
     
-    app.get("/hobbies", function(req, res){
+    app.get('/hobbies', function(req, res){
         
-        res.render("hobbies");
+        res.render('hobbies');
         
     })
     
-    app.get("/contact", function(req, res){
-        res.render("contact");
+    app.get('/contact', function(req, res){
+        res.render('contact');
     })
     
-    app.get("/work", function(req, res){
-        res.render("work");
+    app.get('/work', function(req, res){
+        res.render('work');
     })
     
-   app.get("/image",function(req,res){
-       res.redirect("https://secure-bastion-40206.herokuapp.com/");
+   app.get('/image',function(req,res){
+       res.redirect('https://secure-bastion-40206.herokuapp.com/');
    })
     //Start server
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Project is up");
+    console.log('Project is up');
 });
 
