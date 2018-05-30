f44f765975b95bce92905f30adc69ba78c2577f6
Kervens Jasmin <jakejasminjk@gmail.com>
2018-05-30 04:18:15 +0000

    complete revamp of site

--- app.js
+++ app.js
@@ -1,37 +1,59 @@
-var express     = require("express"),
-    app         = express(),
-    fs          = require("fs"),
-    bodyParser  = require("body-parser");
+let express     = require("express");
+let app         = express();
+let fs          = require("fs");
+let bodyParser  = require("body-parser");
+let nodemailer  = require('nodemailer');
     
-   
-    app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
     
-    app.get('/', function(req, res){
-        res.render('landing')
-    })
     
-    app.get('/home',function(req, res){
+app.get('/',function(req, res){
         
         res.render('home');
-    })
+    });
     
-    app.get('/hobbies', function(req, res){
-        
-        res.render('hobbies');
-        
-    })
-    
-    app.get('/contact', function(req, res){
+app.get('/contact', function(req, res){
         res.render('contact');
-    })
+    });
     
-    app.get('/work', function(req, res){
-        res.render('work');
-    })
+app.get('/about', function(req, res){
+        res.render('about');
+    });
     
-   app.get('/image',function(req,res){
-       res.redirect('https://secure-bastion-40206.herokuapp.com/');
-   })
+app.post('/contact-us', (req,res) => {
+    let name = req.body.name
+    let clientEmail = req.body.email
+    let subject = req.body.subject
+    let message = req.body.message
+    
+    let to = 'CLIENT EMAIL';
+    let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'NEED CLIENTS USERNAME',
+    pass: 'NEED CLIENTS EMAIL PASSWORD'
+  }
+});
+   
+     let mailOptions = {
+        from: clientEmail,
+        to: to, 
+        subject: subject,
+        text: message
+    }
+    transporter.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/home');
+        }
+    });
+    
+ 
+
+})
+    
     //Start server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Project is up');
